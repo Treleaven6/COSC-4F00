995d6f76af1cb60f631954a3174c6737d91fcba1
Jesse Treleaven <jessetreleaven@gmail.com>
2019-03-06 20:49:50 -0500

    Setup Maven & Generated Files from Grammars

---
+++ drjava/src/main/java/ParserTest.java
@@ -0,0 +1,17 @@
+import org.antlr.runtime.ANTLRInputStream;
+
+import java.io.File;
+
+public class ParserTest {
+
+    public static void main(String ... args) {
+
+        File file = new File("C:\\Users\\jesse\\OneDrive\\WorkSpace\\COSC-4F00\\drjava\\src\\main\\java\\HelloWorld.java");
+
+        
+
+    }
+
+
+
+}
