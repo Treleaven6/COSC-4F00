aaddb432f0692d9b27c742758467e8ed65713302
Ji Wang <43019382+HilariousJi@users.noreply.github.com>
2019-03-25 17:49:00 -0400

    Add files via upload

--- server/ListenServer.java
+++ server/ListenServer.java
@@ -3,47 +3,88 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package listenserver;
+package listenclient;
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
+
 
 /**
  *
  * @author Ji Wang_Sager
  */
-public class ListenServer {
+public class ListenServer extends Thread {
 
-    /**
-     * @param args the command line arguments
-     */
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-    
-    public void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        //this part is the actual interaction
-        /*while(true){
-            
-        }*/
+    public static final int PORT = 1234;
+    public static final int BUFFER_SIZE = 100;
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+
+            while (true) {
+                Socket s = serverSocket.accept();
+                saveFile(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
+    private void saveFile(Socket socket) throws Exception {
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        FileOutputStream fos = null;
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        // 1. Read file name.
+        Object o = ois.readObject();
+
+        if (o instanceof String) {
+            fos = new FileOutputStream(o.toString());
+        } else {
+            throwException("Something is wrong");
+        }
+
+        // 2. Read file to the end.
+        Integer bytesRead = 0;
+
+        do {
+            o = ois.readObject();
+
+            if (!(o instanceof Integer)) {
+                throwException("Something is wrong");
+            }
+
+            bytesRead = (Integer) o;
+
+            o = ois.readObject();
+
+            if (!(o instanceof byte[])) {
+                throwException("Something is wrong");
+            }
+
+            buffer = (byte[]) o;
+
+            // 3. Write data to output file.
+            fos.write(buffer, 0, bytesRead);
+
+        } while (bytesRead == BUFFER_SIZE);
+
+        System.out.println("File transfer success");
+
+        fos.close();
+
+        ois.close();
+        oos.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        ListenServer server = new ListenServer();
-        server.start(1234);
+    public static void throwException(String message) throws Exception {
+        throw new Exception(message);
     }
-    
+
+    public static void main(String[] args) {
+        new ListenServer().start();
+    }
 }
